c0b065dece7b85cd7359cad3abff9f4617e884bd
moondeuk <moondeuk@gmail.com>
2023-12-27 17:40:15 -0600

    updated

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            `https://github.com/${websiteConfig.GITHUB_USER}/${websiteConfig.REPOSITORY_NAME}/tree/main/docs`,
+            `https://github.com/${websiteConfig.GITHUB_USER}/${websiteConfig.REPOSITORY_NAME}/tree/main`,
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            `https://github.com/${websiteConfig.GITHUB_USER}/${websiteConfig.REPOSITORY_NAME}/tree/main/blog`,
+            `https://github.com/${websiteConfig.GITHUB_USER}/${websiteConfig.REPOSITORY_NAME}/tree/main`,
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
